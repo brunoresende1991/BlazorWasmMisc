b25856f043e5124abc4fdb0dc6c5542656a1e7ec
Bruno <Bruno@DESKTOP-JDSDNU4>
2021-08-21 17:47:21 -0300

    Adicionar arquivos de projeto.

---
+++ Server/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using BlazorWasm.Server.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorWasm.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BlazorDBContext _context;
+        public BooksController(BlazorDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public object Get()
+        {
+            return new { Items = _context.Book, Count = _context.Book.Count() };
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book book)
+        {
+            _context.Book.Add(book);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<BooksController>
+        [HttpPut]
+        public void Put(long id, [FromBody] Book book)
+        {
+            Book _book = _context.Book.Where(x => x.Id.Equals(book.Id)).FirstOrDefault();
+            _book.Name = book.Name;
+            _book.Author = book.Author;
+            _book.Price = book.Price;
+            _book.Quantity = book.Quantity;
+            _book.Available = book.Available;
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<BooksController>
+        [HttpDelete("{id}")]
+        public void Delete(long id)
+        {
+            Book _book = _context.Book.Where(x => x.Id.Equals(id)).FirstOrDefault();
+            _context.Book.Remove(_book);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Server/Models/BlazorDBContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlazorWasm.Server.Models
+{
+    public partial class BlazorDBContext : DbContext
+    {
+        public BlazorDBContext()
+        {
+        }
+
+        public BlazorDBContext(DbContextOptions<BlazorDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Author)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
